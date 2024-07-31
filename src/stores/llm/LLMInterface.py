3c38913be81453e5c6abda3ae2d976bcb552d8be
MennaHefny25 <mennahefny25@domain.com>
2024-07-31 09:41:48 +0200

    create llmenums & inteface

--- src/stores/llm/LLMInterface.py
+++ src/stores/llm/LLMInterface.py
@@ -9,21 +9,21 @@ class LLMInterface(ABC):
 
 
     @abstractmethod
-    def set_embedding_model(self, model_id: str):
+    def set_embedding_model(self, model_id: str, embedding_size: int):
         pass
 
 
     @abstractmethod
-    def generate_text(self, prompt: str, max_output_token: str, 
-                      temperature: float=None): # temperature for creativity
+    def generate_text(self, prompt: str, chat_history: list=[], max_output_tokens: int=None,
+                            temperature: float = None):
         pass
 
     @abstractmethod
-    def embed_text(self, text: str, document_type: str): # temperature for creativity
+    def embed_text(self, text: str, document_type: str = None): 
         pass
 
     @abstractmethod
-    def construct_prompt(self, prompt: str, role: str): # temperature for creativity
+    def construct_prompt(self, prompt: str, role: str): 
         pass
 
 
