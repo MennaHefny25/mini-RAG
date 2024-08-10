849fa80787f35e921d53a785935acb420c76e2f9
MennaHefny25 <mennahefny25@gmail.com>
2024-08-10 22:02:50 +0300

    Semantic Search

--- src/stores/vectordb/providers/QdrantDBProvider.py
+++ src/stores/vectordb/providers/QdrantDBProvider.py
@@ -4,7 +4,6 @@ from ..VectorDBEnums import DistanceMethodEnums
 import logging
 from typing import List
 
-
 class QdrantDBProvider(VectorDBInterface):
 
     def __init__(self, db_path: str, distance_method: str):
@@ -28,50 +27,44 @@ class QdrantDBProvider(VectorDBInterface):
 
     def is_collection_existed(self, collection_name: str) -> bool:
         return self.client.collection_exists(collection_name=collection_name)
-
+    
     def list_all_collections(self) -> List:
         return self.client.get_collections()
-
+    
     def get_collection_info(self, collection_name: str) -> dict:
         return self.client.get_collection(collection_name=collection_name)
-
+    
     def delete_collection(self, collection_name: str):
         if self.is_collection_existed(collection_name):
             return self.client.delete_collection(collection_name=collection_name)
-
-    def create_collection(
-        self, collection_name: str, embedding_size: int, do_reset: bool = False
-    ):
+        
+    def create_collection(self, collection_name: str, 
+                                embedding_size: int,
+                                do_reset: bool = False):
         if do_reset:
             _ = self.delete_collection(collection_name=collection_name)
-
+        
         if not self.is_collection_existed(collection_name):
             _ = self.client.create_collection(
                 collection_name=collection_name,
                 vectors_config=models.VectorParams(
-                    size=embedding_size, distance=self.distance_method
-                ),
+                    size=embedding_size,
+                    distance=self.distance_method
+                )
             )
 
             return True
-
+        
         return False
-
-    def insert_one(
-        self,
-        collection_name: str,
-        text: str,
-        vector: list,
-        metadata: dict = None,
-        record_id: str = None,
-    ):
-
+    
+    def insert_one(self, collection_name: str, text: str, vector: list,
+                         metadata: dict = None, 
+                         record_id: str = None):
+        
         if not self.is_collection_existed(collection_name):
-            self.logger.error(
-                f"Can not insert new record to non-existed collection: {collection_name}"
-            )
+            self.logger.error(f"Can not insert new record to non-existed collection: {collection_name}")
             return False
-
+        
         try:
             _ = self.client.upload_records(
                 collection_name=collection_name,
@@ -79,26 +72,22 @@ class QdrantDBProvider(VectorDBInterface):
                     models.Record(
                         id=[record_id],
                         vector=vector,
-                        payload={"text": text, "metadata": metadata},
+                        payload={
+                            "text": text, "metadata": metadata
+                        }
                     )
-                ],
+                ]
             )
         except Exception as e:
             self.logger.error(f"Error while inserting batch: {e}")
             return False
 
         return True
-
-    def insert_many(
-        self,
-        collection_name: str,
-        texts: list,
-        vectors: list,
-        metadata: list = None,
-        record_ids: list = None,
-        batch_size: int = 50,
-    ):
-
+    
+    def insert_many(self, collection_name: str, texts: list, 
+                          vectors: list, metadata: list = None, 
+                          record_ids: list = None, batch_size: int = 50):
+        
         if metadata is None:
             metadata = [None] * len(texts)
 
@@ -117,8 +106,11 @@ class QdrantDBProvider(VectorDBInterface):
                 models.Record(
                     id=batch_record_ids[x],
                     vector=batch_vectors[x],
-                    payload={"text": batch_texts[x], "metadata": batch_metadata[x]},
+                    payload={
+                        "text": batch_texts[x], "metadata": batch_metadata[x]
+                    }
                 )
+
                 for x in range(len(batch_texts))
             ]
 
@@ -132,9 +124,11 @@ class QdrantDBProvider(VectorDBInterface):
                 return False
 
         return True
-
+        
     def search_by_vector(self, collection_name: str, vector: list, limit: int = 5):
 
         return self.client.search(
-            collection_name=collection_name, query_vector=vector, limit=limit
+            collection_name=collection_name,
+            query_vector=vector,
+            limit=limit
         )
